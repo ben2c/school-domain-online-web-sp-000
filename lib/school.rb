8641e6eb27f8bfd006c6b537c92f8476d60ba079
Benjamin Cheng <bencheng200@gmail.com>
2020-11-15 19:37:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,14 @@
 # code here!
 class School
-  attr_reader :name :@roster
+  attr_reader :name 
 
   def initialize(name)
     @name = name
     @roster = {}
   end
 
-
+  def roster
+    @roster
+  end
 
 end
